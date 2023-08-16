c483de67684ce8ac6b5252c2ba9f9c547eb94e5b
Zoyisiwe <zoyisiwemakhunga@gmail.com>
2023-08-16 17:56:17 +0200

    SQL 2

---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,38 @@
+-- a script that creates the database hbtn_0c_0 in your MySQL server.
+
+import mysql.connector
+
+# Replace these with your MySQL server credentials
+hostname = "localhost"
+username = "your_username"
+password = "your_password"
+
+try:
+    # Connect to the MySQL server
+    connection = mysql.connector.connect(
+        host=hostname,
+        user=username,
+        password=password
+    )
+
+    # Create a cursor object to interact with the server
+    cursor = connection.cursor()
+
+    # Execute the SQL query to create the database if it doesn't exist
+    query = "CREATE DATABASE IF NOT EXISTS hbtn_0c_0"
+    cursor.execute(query)
+
+    # Commit the changes
+    connection.commit()
+
+    print("Database hbtn_0c_0 created or already exists.")
+
+except mysql.connector.Error as err:
+    print("Error:", err)
+
+finally:
+    # Close the cursor and connection
+    if cursor:
+        cursor.close()
+    if connection.is_connected():
+        connection.close()
