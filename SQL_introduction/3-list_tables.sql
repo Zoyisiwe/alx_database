4ef27ea253ff599da1226a3e911544af66a976e9
Zoyisiwe <zoyisiwemakhunga@gmail.com>
2023-08-17 18:01:49 +0200

    SQL 4

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,3 +1,5 @@
 -- a script that lists all the tables of a database in your MySQL server.
 
-SHOW TABLES IN mysql
+SELECT TABLE_NAME
+FROM INFORMATION_SCHEMA.TABLES
+WHERE TABLE_SCHEMA = 'your_database_name';
