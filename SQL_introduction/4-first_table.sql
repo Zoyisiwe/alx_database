1c127995158999cb8500dbeaa3dba998ee80e6de
Zoyisiwe <zoyisiwemakhunga@gmail.com>
2023-08-17 18:09:09 +0200

    SQL 5

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,6 @@
 --a script that creates a table called first_table in the current database in your MySQL server.
 
-CREATE TABLE IF NOT EXISTS first_table (
+CREATE TABLE IF NOT EXISTS hbtn_test_db_4.first_table (
     id INT,
     name VARCHAR(256)
     );
