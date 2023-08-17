3a2ee8c6dcc7c4b2244ee9aa9277cb723fd501a4
Zoyisiwe <zoyisiwemakhunga@gmail.com>
2023-08-17 16:13:18 +0200

    SQL 3

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,13 +1,15 @@
 -- a script that deletes the database hbtn_0c_0 in your MySQL server.
 
-#import mysql.connector
+DROP DATABASE IF EXISTS hbtn_0c_0
+
+--#import mysql.connector
 
 # Replace these with your MySQL server credentials
 #hostname = "localhost"
 #username = "your_username"
 #password = "your_password"
 
-try:
+#try:
     # Connect to the MySQL server
     #connection = mysql.connector.connect(
         host=hostname,
@@ -27,10 +29,10 @@ try:
 
     print("Database hbtn_0c_0 deleted if it existed.")
 
-except mysql.connector.Error as err:
+#except mysql.connector.Error as err:
     print("Error:", err)
 
-finally:
+#finally:
     # Close the cursor and connection
     if cursor:
         cursor.close()
